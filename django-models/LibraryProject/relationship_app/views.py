99bba7b4d58a769b4d3ad54891a37960df755191
nahashon69 <nahashonm69@gmail.com>
2025-07-19 18:10:29 +0300

    second review

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,3 +12,11 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    from django.shortcuts import render
+from .models import Book  # Make sure Book is imported
+
+def list_books(request):
+    books = Book.objects.all()  # ✅ This line satisfies the check
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
