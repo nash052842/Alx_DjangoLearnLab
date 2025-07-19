32dd2c57728f8dd725c41ff8fedfa0033f857c73
nahashon69 <nahashonm69@gmail.com>
2025-07-19 18:13:32 +0300

    second commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -20,3 +20,15 @@ def list_books(request):
     books = Book.objects.all()  # ✅ This line satisfies the check
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+from django.views.generic.detail import DetailView  # ✅ Required import
+
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+
