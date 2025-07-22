968c3e99a9e9c7044e81f9bfde4d9bb8f28bb857
nahashon69 <nahashonm69@gmail.com>
2025-07-22 19:22:30 +0300

    good

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 class Book(models.Model):
@@ -6,7 +7,10 @@ class Book(models.Model):
     publication_year = models.IntegerField()
 
     def __str__(self):
+        
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+
 
 
 
