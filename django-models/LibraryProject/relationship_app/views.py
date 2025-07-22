589942811719c6c2bab4e475d96f9c3e3fc75162
nahashon69 <nahashonm69@gmail.com>
2025-07-22 22:48:34 +0300

    good

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Library
+from django.views.generic.detail import DetailView  # for CBV
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-Based View to show a specific library with its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
