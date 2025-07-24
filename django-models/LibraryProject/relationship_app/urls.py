2fae3d01705b25fc7795506c743d0b616d4c728e
nahashon69 <nahashonm69@gmail.com>
2025-07-24 22:00:08 +0300

    good

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -50,4 +50,13 @@ urlpatterns = [
 ]
 
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/add/', views.add_book, name='add_book'),           
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),  
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('books/', views.list_books, name='list_books'),
+]
 
