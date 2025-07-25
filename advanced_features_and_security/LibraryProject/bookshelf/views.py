80ba8d20a9dbea8303c51cb06f942e2a380cc9d1
nahashon69 <nahashonm69@gmail.com>
2025-07-25 23:47:59 +0300

    good

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -65,3 +65,13 @@ def example_view(request):
     
     return render(request, 'bookshelf/example_form.html', {'form': form})
 
+from django.shortcuts import render
+from .models import Book
+from django.contrib.auth.decorators import permission_required
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
