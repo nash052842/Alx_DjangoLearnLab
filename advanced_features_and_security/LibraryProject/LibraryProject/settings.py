d90d8cf9a13e9e97f358ca4e185fe16c668bf80d
nahashon69 <nahashonm69@gmail.com>
2025-07-25 22:20:47 +0300

    good

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.CustomUser',
+    'bookshelf',
     'relationship_app'
     'accounts.customUser',
 ]
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,25 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+
+
+
+# accounts/admin.py
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    admin.site.register(CustomUser,CustomUserAdmin)
