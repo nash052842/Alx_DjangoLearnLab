27cd13aeccb5ab83f3bb5dd29fc048e4236b6ff9
nahashon69 <nahashonm69@gmail.com>
2025-07-25 22:24:58 +0300

    good

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -27,4 +27,5 @@ class CustomUserAdmin(UserAdmin):
     )
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
     search_fields = ('username', 'email', 'first_name', 'last_name')
-    admin.site.register("CustomUser,CustomUserAdmin")
+    
+admin.site.register(CustomUser,CustomUserAdmin)
