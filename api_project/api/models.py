9ab8657ac5e3344e2852b594e477226cbb39a992
nahashon69 <nahashonm69@gmail.com>
2025-07-28 23:12:09 +0300

    more corections in modesl.py

---
+++ api_project/api/models.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+
+class Book (models.Models):
+    title= models.CharField(max_length=200)
+
+    author= models.CharField(max_length=100)
+
+def __str__(self):
+
+    return f"{self.title} by {self.author}"
+
+
+
