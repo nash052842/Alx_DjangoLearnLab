3ffd33d78b42c201785fb19f9dffb8692c92e832
nahashon69 <nahashonm69@gmail.com>
2025-07-29 00:08:13 +0300

    correction in serializer

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from.models import Book
 from api import models
 
-class BookSerializer(serializers.Modelserializer):
+class BookSerializer(serializers.ModelSerializer):
     class meta:
         model=Book
         field="__all__"
