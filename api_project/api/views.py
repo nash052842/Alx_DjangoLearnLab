4a170599c8bad3eb8e3e61f61ac9f68f177d2fdc
nahashon69 <nahashonm69@gmail.com>
2025-07-29 22:41:52 +0300

    cerretion

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,10 +8,11 @@ class BookList(generics.ListAPIView):
     queryset =Book.object.all()
 
     serilaizer_class = BookSerializer
+
     
 from rest_framework import serializer
 from .models import Book
-class Bookserializer(serializers.ModelSerializer):
+class Bookserializer(serializer.ModelSerializer):
     
     class meta:
         model=Book
@@ -21,6 +22,6 @@ from rest_framework import viewsets
 from .models import Book
 from serializers import Bookserializer
 
-classBookviewset = (viewsets.Modelkviewset):
-queryset= Book.object.all()
+class Bookviewset = (viewsets.Modelkviewset):
+queryset = Book.object.all()
 serializer_Class= BookSerializer
