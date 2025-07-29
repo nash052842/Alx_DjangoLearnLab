6e0d41fb216d5713a387952381d480deb662d51a
nahashon69 <nahashonm69@gmail.com>
2025-07-29 22:38:26 +0300

    CRUD operatin

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,3 +4,15 @@ from .views import BookList
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 ]
+
+from django , urls import path, include
+from rest_framework.import DefaultRounter
+from.views import BookViewSet
+
+router=DefaultRounter()
+
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns= [
+    path(''inlude(router urls)),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,3 +9,18 @@ class BookList(generics.ListAPIView):
 
     serilaizer_class = BookSerializer
     
+from rest_framework import serializer
+from .models import Book
+class Bookserializer(serializers.ModelSerializer):
+    
+    class meta:
+        model=Book
+        field = ["id","title","author","publish_date"]
+
+from rest_framework import viewsets
+from .models import Book
+from serializers import Bookserializer
+
+classBookviewset = (viewsets.Modelkviewset):
+queryset= Book.object.all()
+serializer_Class= BookSerializer
