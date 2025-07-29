1a0c8ef0e6c0bf0454e9ec6ef140cb16d9070334
nahashon69 <nahashonm69@gmail.com>
2025-07-29 22:49:37 +0300

    correction

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -22,6 +22,6 @@ from rest_framework import viewsets
 from .models import Book
 from serializers import Bookserializer
 
-class BookViewSet(viewsets.Modelkviewset):
+class BookViewSet(viewsets.Modelviewset):
 queryset = Book.objects.all()
 serializer_Class= BookSerializer
