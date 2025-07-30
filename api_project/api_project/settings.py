6ebadba6ad0731345a85751f25020aa7a2e016c8
nahashon69 <nahashonm69@gmail.com>
2025-07-30 20:33:10 +0300

    update Adminuser

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        
+        'rest_framework.permissions.IsAuthenticate'
     ]
 }
 
