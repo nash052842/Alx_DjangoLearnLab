104bb13426d36977be635a7ecc2a156183e481b0
nahashon69 <nahashonm69@gmail.com>
2025-07-31 19:36:37 +0300

    settings.py correction

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from api.models import Book
+from api.serializers import BookSerializer
+from api.views import BookList
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
