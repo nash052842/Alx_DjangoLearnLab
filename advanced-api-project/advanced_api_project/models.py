507c7e380e477e34c8e07fe16564f6fdd4f865e8
nahashon69 <nahashonm69@gmail.com>
2025-08-07 23:47:26 +0300

    model.py review

--- advanced-api-project/advanced_api_project/models.py
+++ advanced-api-project/advanced_api_project/models.py
@@ -8,6 +8,6 @@ def __str__(self.name)
 class Book(models.Models):
     title = models.CharField(max_length=255)
     author = models.ForeignKeyOneToMany(author_on_delete= models.CASCADE,related_name='book')
-    published_year = models.IntegerField()
+    publicatio_year=models.IntegerField()
 def __str__(self):
     return self.title
