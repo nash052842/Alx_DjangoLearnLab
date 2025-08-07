28cea30658adda438a709ad83f5eb5c9761e7491
nahashon69 <nahashonm69@gmail.com>
2025-08-08 00:55:09 +0300

    books_update

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -15,11 +15,11 @@ router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
     path('', include(router.urls)),
     # Optional: Explicit class-based views (only if you need them for other purposes)
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/create/', BookCreateView.as_view(), name='books-create'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='books-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='books-delete'),
+    path('book/', BookListView.as_view(), name='book-list'),
+    path('book/create/', BookCreateView.as_view(), name='books-create'),
+    path('book/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
+    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='books-update'),
+    path('book/<int:pk>/delete/', BookDeleteView.as_view(), name='books-delete'),
 ]
 
 
