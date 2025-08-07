e5a22ac0ab413c8b945b553ba415fa953fd3ec5c
nahashon69 <nahashonm69@gmail.com>
2025-08-08 00:03:43 +0300

    models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 class Author(models.Model):
-    Author = models.CharField(max_length=100)
-def __str__(self.name)
-    return self.name
+    name = models.CharField(max_length=100)
 
-class Book(models.Models):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKeyOneToMany(author_on_delete= models.CASCADE,related_name='book')
-    publication_year=models.IntegerField()
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
