6d09bc84871e89f1870c1961a91dd3c3d511857e
nahashon69 <nahashonm69@gmail.com>
2025-08-11 13:43:02 +0300

    CRUD operation files

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,25 +1,30 @@
 from django.urls import path
 from.import views
-(
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-)
 
 urlpattern=[
     path('register/' views.register_view,name='register')
     path('login/'views.login_view,name='login')
     path('logout/'views.logout_view,name='logout')
     path('profile/' views.profile_view,name= 'profile')
-
 ]
+
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
-    path('posts/',PostListView.as_view(), name='post-list'),
-    path('posts/new/',PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/',PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
+    path('', PostListView.as_view(), name='post-list'),  # List all posts
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View one post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Update a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 ]
+
+
+
+
