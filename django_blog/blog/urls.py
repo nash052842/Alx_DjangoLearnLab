9f38cc5d797eed5bc61c19b1e3317dcb9d73b368
nahashon69 <nahashonm69@gmail.com>
2025-08-11 13:38:45 +0300

    permission enforced with Django mixixns for security

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,9 +17,9 @@ urlpattern=[
 ]
 
 urlpatterns = [
-    path('posts/',views.PostListView.as_view(), name='post-list'),
-    path('posts/new/',views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('posts/',PostListView.as_view(), name='post-list'),
+    path('posts/new/',PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/',PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
 ]
