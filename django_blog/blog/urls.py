54a415c4f456b426032e9884c6f2b70327b3ba7d
nahashon69 <nahashonm69@gmail.com>
2025-08-11 19:09:46 +0300

    file review

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     path('post/<int:post_pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    # This is the new URL to add for comment creation
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
 ]
