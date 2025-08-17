7445fbd3d0ca4d0daac61e7d887d9c4c52a11bd2
nahashon69 <nahashonm69@gmail.com>
2025-08-17 14:24:22 +0300

    blog/url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 
     # ---------- COMMENT ROUTES ----------
-path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-path('post/<int:pk>/comments/<int:comment_pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-path('post/<int:pk>/comments/<int:comment_pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/<int:comment_pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('post/<int:pk>/comments/<int:comment_pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 from .views import SearchResultsView
 from taggit.views import TaggedObjectList
