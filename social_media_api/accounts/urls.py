dbfcdbaecac7fce84ff4c6837fa8f5875d3269e7
nahashon69 <nahashonm69@gmail.com>
2025-08-20 20:05:36 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,6 +2,9 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from posts.views import PostViewSet, CommentViewSet
 from django.urls import path
+from django.urls import path
+from accounts.views import FollowUserView, UnfollowUserView
+
 from .views import (
     ProfileView,
     loginViewSet,
@@ -21,8 +24,12 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = router.urls + [
     path('users/<int:pk>/followers/', FollowUserView.as_view(), name='follow-user'),
     path('users/<int:pk>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path
     path('users/me/', ProfileView.as_view(), name='user-profile'),
     path('users/login/', loginViewSet.as_view({'post': 'create'}), name='user-login'),
     path('users/logout/', loginViewSet.as_view({'post': 'destroy'}), name='user-logout'),
     path('users/register/', CustomUserViewSet.as_view({'post': 'create'}), name='user-register'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+
 ]
