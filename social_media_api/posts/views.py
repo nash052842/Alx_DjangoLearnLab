265fc4381bc8200d4edf9ae427f92909d93a15ad
nahashon69 <nahashonm69@gmail.com>
2025-08-21 20:31:24 +0300

    post/view.py amended

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,20 +1,20 @@
-from rest_framework import viewsets, filters
+
+from rest_framework import viewsets, filters, permissions
 from rest_framework.permissions import IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import permissions
-from posts.models import Post
-from posts.serializers import PostSerializer
-from .models import Comment, Post
-from .permissions import IsOwnerOrReadOnly
-from .serializers import CommentSerializer, PostSerializer
-from notifications.models import Notification
-from notifications.utils import create_notification
+from django_filters.rest_framework import DjangoFilterBackend
 
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
+
+from posts.models import Post, Comment, Like
+from posts.serializers import PostSerializer, CommentSerializer
+from posts.permissions import IsOwnerOrReadOnly
+
+from notifications.models import Notification
+from notifications.utils import create_notification
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -33,17 +33,21 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        comment = serializer.save(author=self.request.user)
+        Notification.objects.create(
+            recipient=comment.post.author,
+            sender=self.request.user,
+            notification_type='comment',
+            post=comment.post
+        )
+
 
 class FeedView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, format=None):
-        
         following_users = request.user.profile.following.all()
-        
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')[:10]
-        
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
 
@@ -55,51 +59,19 @@ class FeedView(APIView):
         return Response(serializer.errors, status=400)
 
 
-# posts/views.py
-
-def perform_create(self, serializer):
-    comment = serializer.save(author=self.request.user)
-    Notification.objects.create(
-        recipient=comment.post.author,
-        sender=self.request.user,
-        notification_type='comment',
-        post=comment.post
-    )
-
-
-
-from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Post, Like
-from notifications.utils import create_notification  # You'll define this next
-
 @login_required
 def like_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     like, created = Like.objects.get_or_create(user=request.user, post=post)
-    if created:
-        if post.author != request.user:
-            create_notification(
-                recipient=post.author,
-                actor=request.user,
-                verb="liked your post",
-                target=post
-            )
+    if created and post.author != request.user:
+        create_notification(
+            recipient=post.author,
+            actor=request.user,
+            verb="liked your post",
+            target=post
+        )
     return redirect('post_detail', pk=pk)
 
-@login_required
-def like_post(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    like, created = Like.objects.get_or_create(user=request.user, post=post)
-    if created:
-        if post.author != request.user:
-            create_notification(
-                recipient=post.author,
-                actor=request.user,
-                verb="liked your post",
-                target=post
-            )
-    return redirect('post_detail', pk=pk)
 
 @login_required
 def unlike_post(request, pk):
